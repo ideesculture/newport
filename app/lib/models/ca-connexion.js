207e878eb45d5a5cd00214877753287ece7bcbfe
GautierMichelin <gm@ideesculture.com>
2014-08-28 17:12:54 +0200

    Removing code cleanup on compile

--- app/lib/models/ca-connexion.js
+++ app/lib/models/ca-connexion.js
@@ -8,25 +8,25 @@
 exports.definition = {
 
 	config: {
-	    	adapter: {
+		adapter: {
 			type: "ti-collectiveaccess",
 			collection_name: "docs",
 			base_url: "http://fakerest.site.192.168.20.10.xip.io/login_ok.txt"
 		}
 	},
-	
+
 	extendModel: function(Model) {
 		_.extend(Model.prototype, {
-			
-			idAttribute : "_id",
-			
+
+			idAttribute: "_id",
+
 			validate: function(attrs) {
 				// add here validation for single attributes returning errors 
 			}
 		});
 		return Model;
 	},
-	
+
 	extendCollection: function(Collection) {
 		_.extend(Collection.prototype, {
 			// extended functions and properties go here
@@ -35,5 +35,3 @@ exports.definition = {
 	}
 
 };
-
-
