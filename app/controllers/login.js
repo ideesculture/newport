05a50e19564b129c294c2f5eb8a87905eb2076ee
GautierMichelin <gm@ideesculture.com>
2014-08-28 14:04:21 +0200

    Login screen

---
+++ app/controllers/login.js
@@ -0,0 +1,20 @@
+var APP = require("core");
+
+$.init = function() {
+	APP.log("debug", "settings.init");
+
+	$.NavigationBar.setBackgroundColor(APP.Settings.colors.primary);
+
+	if(APP.Settings.useSlideMenu) {
+		$.NavigationBar.showMenu(function(_event) {
+			APP.toggleMenu();
+		});
+	} else {
+		$.NavigationBar.showBack(function(_event) {
+			APP.removeChild(true);
+		});
+	}
+};
+
+// Kick off the init
+$.init();
