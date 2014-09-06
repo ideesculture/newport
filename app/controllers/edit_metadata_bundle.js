b09f171331664f6fe288a33359ef457004cbb029
GautierMichelin <gm@ideesculture.com>
2014-09-06 13:39:33 +0200

    Expand display limit (10 to 25), simple toggle

--- app/controllers/edit_metadata_bundle.js
+++ app/controllers/edit_metadata_bundle.js
@@ -5,81 +5,54 @@ $.bundleItemName.text = CONFIG.content.name;
 $.bundleItemName.bak = {height : 0};
 
 // by default the panel bundleItemElements is not deployed (invisible)
-//$.bundleItemElements.visible = false;
-/*
+$.bundleItemElements.height = 1;
+$.bundleItemElements.visible = false;
+
 $.bundleItem.addEventListener("click", function(_event) {
-	//APP.log("debug","$.bundleItemForm.getHeight()");
-	APP.log("debug",$.bundleItemForm.getHeight());
-	/*
-	var dialog = Ti.UI.createAlertDialog({
-	    message: CONFIG.element+" clicked",
-	    ok: 'OK',
-	    title: 'Click'
-	  }).show();
-	  *//*
+
 	if ($.bundleItemElements.visible == true) {
-		$.bundleItemForm.height="340dp";
-		$.bundleItemElements.animate({
-			height: "1dp",
-			duration:500
-		}, function() {
-			$.bundleItemForm.animate({
-				height: "40dp",
-				duration:500
-			});
-			$.bundleItemElements.visible = false;		
-		});
-		$.deployed=false;
+		$.bundleItemElements.visible = false;
+		$.bundleItemElements.height = 1;
 	} else {
-		$.bundleItemElements.visible = true;		
-		$.bundleItemForm.height="340dp";
-		$.bundleItemElements.animate({
-			height : "340dp",
-			duration:500
-		});
-		$.deployed = true;
+		$.bundleItemElements.visible = true;
+		$.bundleItemElements.height = Ti.UI.SIZE;
 	}
 	
 });
-*/
+
 // Create fields inside the bundle
 var height=0;
 var i = 1;
-$.bundleItemElements.setOpacity(0);
+var datatypeControllers = {
+	"Text":"edit_metadata_element_text",
+	"Currency":"edit_metadata_element_currency",
+	"Numeric":"edit_metadata_element_numeric",
+	"Text":"edit_metadata_element_text",
+	"Url":"edit_metadata_element_url",
+	"Length":"edit_metadata_element_length",
+	"Weight":"edit_metadata_element_weight"
+};
 for(var element in CONFIG.content.elements_in_set) {
 	var content = CONFIG.content.elements_in_set[element];
-	APP.log("debug","element");
-	APP.log("debug",element);
-	//APP.log("debug",CONFIG.content.elements_in_set[element]);
 
-	if(i<10) {
+	if(i<25) {
 		// if element.datatype == "Text" => formulaire texte, else...
 		APP.log("debug",element.datatype);
-		switch(content.datatype) {
-		    case "Text":
-		        var row = Alloy.createController("edit_metadata_element_text", {
-					element:element,
-					content:CONFIG.content.elements_in_set[element]
-				}).getView();
-				
-		        break;
-		    case "Length":
-		        var row = Alloy.createController("edit_metadata_element_length", {
-					element:element,
-					content:CONFIG.content.elements_in_set[element]
-				}).getView();
-		        break;
-		    default:
-		    	var row = Alloy.createController("edit_metadata_element_unsupported", {
-					element:element,
-					content:CONFIG.content.elements_in_set[element]
-				}).getView();
-		        break;
+		if(content.datatype in datatypeControllers) {
+    		// does exist
+			var row = Alloy.createController(datatypeControllers[content.datatype], {
+				element:element,
+				content:CONFIG.content.elements_in_set[element]
+			}).getView();
+		} else {
+    		var row = Alloy.createController("edit_metadata_element_unsupported", {
+				element:element,
+				content:CONFIG.content.elements_in_set[element]
+			}).getView();
 		}
+		
 		$.bundleItemElements.add(row);
 		
 	}
 	i++;
 };
-
-$.bundleItemElements.setOpacity(1);
