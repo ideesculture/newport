80b41c6fc0459523a40b4c04d3a871dca5edd441
GautierMichelin <gm@ideesculture.com>
2014-09-06 09:59:40 +0200

    Temporary removing slide down animation

--- app/controllers/edit_metadata_bundle.js
+++ app/controllers/edit_metadata_bundle.js
@@ -5,8 +5,8 @@ $.bundleItemName.text = CONFIG.content.name;
 $.bundleItemName.bak = {height : 0};
 
 // by default the panel bundleItemElements is not deployed (invisible)
-$.bundleItemElements.visible = false;
-
+//$.bundleItemElements.visible = false;
+/*
 $.bundleItem.addEventListener("click", function(_event) {
 	//APP.log("debug","$.bundleItemForm.getHeight()");
 	APP.log("debug",$.bundleItemForm.getHeight());
@@ -16,9 +16,9 @@ $.bundleItem.addEventListener("click", function(_event) {
 	    ok: 'OK',
 	    title: 'Click'
 	  }).show();
-	  */
+	  *//*
 	if ($.bundleItemElements.visible == true) {
-		$.bundleItemForm.height="140dp";
+		$.bundleItemForm.height="340dp";
 		$.bundleItemElements.animate({
 			height: "1dp",
 			duration:500
@@ -32,38 +32,54 @@ $.bundleItem.addEventListener("click", function(_event) {
 		$.deployed=false;
 	} else {
 		$.bundleItemElements.visible = true;		
-		$.bundleItemForm.height="140dp";
+		$.bundleItemForm.height="340dp";
 		$.bundleItemElements.animate({
-			height : "140dp",
+			height : "340dp",
 			duration:500
 		});
 		$.deployed = true;
 	}
 	
 });
-
-// Form rows inside the metadata
-var rows=[];
+*/
+// Create fields inside the bundle
+var height=0;
 var i = 1;
+$.bundleItemElements.setOpacity(0);
 for(var element in CONFIG.content.elements_in_set) {
+	var content = CONFIG.content.elements_in_set[element];
 	APP.log("debug","element");
 	APP.log("debug",element);
-	APP.log("debug",CONFIG.content.elements_in_set[element]);
+	//APP.log("debug",CONFIG.content.elements_in_set[element]);
 
 	if(i<10) {
 		// if element.datatype == "Text" => formulaire texte, else...
-		var row = Alloy.createController("edit_metadata_element", {
-			element:element,
-			content:CONFIG.content.elements_in_set[element]
-		}).getView();
-		rows.push(row);
+		APP.log("debug",element.datatype);
+		switch(content.datatype) {
+		    case "Text":
+		        var row = Alloy.createController("edit_metadata_element_text", {
+					element:element,
+					content:CONFIG.content.elements_in_set[element]
+				}).getView();
+				
+		        break;
+		    case "Length":
+		        var row = Alloy.createController("edit_metadata_element_length", {
+					element:element,
+					content:CONFIG.content.elements_in_set[element]
+				}).getView();
+		        break;
+		    default:
+		    	var row = Alloy.createController("edit_metadata_element_unsupported", {
+					element:element,
+					content:CONFIG.content.elements_in_set[element]
+				}).getView();
+		        break;
+		}
+		$.bundleItemElements.add(row);
+		
 	}
 	i++;
 };
 
-$.bundleItemElements.setOpacity(0);
-$.bundleItemElements.setData(rows);
-for(var x=0; x<rows.length; x++) {
-	$.bundleItemElements.add(rows[x]);
-}
 $.bundleItemElements.setOpacity(1);
