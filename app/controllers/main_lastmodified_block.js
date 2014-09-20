8c4c6e346f96b64b34b9a7d12052c2edfcac7c0e
GautierMichelin <gm@ideesculture.com>
2014-09-20 14:16:51 +0200

    Defining lastmodified blocks text from model

--- app/controllers/main_lastmodified_block.js
+++ app/controllers/main_lastmodified_block.js
@@ -6,11 +6,17 @@
  */
 var APP = require("core");
 var CONFIG = arguments[0];
+APP.log("debug","Adding lastmodified block: "+CONFIG.label1+" ("+CONFIG.created+")");
+APP.log("debug","Date(CONFIG.created)");
+APP.log("debug",CONFIG.created);
+APP.log("debug",Date(CONFIG.created));
 
 // Adding recent items to right panel
 //var view = Alloy.createController("main_lastmodified_block_folderrow", {id:43}).getView();
-$.itemTreeTable.setHeight(120);
-$.itemTreeTable.setWidth(50);
-$.itemTreeTable.setBackgroundColor("yellow");
 //APP.log("debug",$.itemTreeTable.children);
+$.label4.text = CONFIG.label4;
+$.label3.text = CONFIG.label3;
+$.label2.text = CONFIG.label2;
+$.label1.text = CONFIG.label1;
+$.rightDate.text = Date(CONFIG.created);
 //$.objectTable.add(view);
