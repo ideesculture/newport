0b44efbe5aadcf7ab0a13f7a4c8ffc133eb7c3e8
GautierMichelin <gm@ideesculture.com>
2014-09-20 23:17:48 +0200

    main_folders_row : Ctr/Vw/St

---
+++ app/controllers/main_folders_row.js
@@ -0,0 +1,23 @@
+/**
+ * Controller for a last modified block in the right panel
+ * 
+ * @class Controllers.text
+ * @uses core
+ */
+var APP = require("core");
+var CONFIG = arguments[0];
+
+APP.log("debug","Adding folder row ("+CONFIG.object_id+")");
+
+$.foldersItemName.text = CONFIG.display_label;
+$.foldersItemCount.text = "("+CONFIG.contains+")";
+
+$.foldersItem.addEventListener('click',function(e) {
+	APP.log("debug", "adding new child (main.js): "+CONFIG.object_id);
+	var child_info = {
+		id: CONFIG.object_id,
+		display_label: CONFIG.display_label
+	}
+	APP.addChild("main", child_info, false, false);
+	APP.breadcrumb.push(child_info);
+});
