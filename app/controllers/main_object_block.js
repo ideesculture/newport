b463da50d5741ae3cb66395cf95907046aee3b25
GautierMichelin <gm@ideesculture.com>
2014-09-20 23:18:21 +0200

    Object block : Ctr/Vw/St

---
+++ app/controllers/main_object_block.js
@@ -0,0 +1,10 @@
+/**
+ * Controller for an object block bottom of the screen
+ * 
+ * @class Controllers.text
+ * @uses core
+ */
+var APP = require("core");
+var CONFIG = arguments[0];
+
+APP.log("debug","Adding object block ("+CONFIG.object_id+")");
