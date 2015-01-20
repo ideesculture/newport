420c4bdce124e39ae7d6abddca24c349586b5ccd
GautierMichelin <gm@ideesculture.com>
2015-01-20 06:29:26 +0100

    edit buffer

---
+++ app/lib/ca-editbuffer.js
@@ -0,0 +1,17 @@
+
+var Alloy = require("alloy");
+var UTIL = require("utilities");
+var HTTP = require("http");
+
+var BUFFER = {
+	/**
+	 * NEW VALUES STORAGE
+	 */
+	VALUES: {},
+	/**
+	 * ORGINAL VALUES STORAGE
+	 */
+	ORIGINAL: {}
+};
+
+module.exports = BUFFER;
