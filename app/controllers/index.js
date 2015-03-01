6b93a03fa1c136ae9b69ec16cfb95b0d81a7c751
GautierMichelin <gm@ideesculture.com>
2015-03-01 20:40:38 +0100

    adding test fairy token

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -11,6 +11,7 @@
 
 // Pull in the core APP singleton
 var APP = require("core");
+require('com.arihiro.titestfairy').begin("b911dae286a68622d9e1cb8fb15ee847b85d27fc");
 
 // Make sure we always have a reference to global elements throughout the APP singleton
 APP.MainWindow = $.MainWindow;
