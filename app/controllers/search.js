2cd1302f00614d798904d2d7e0c6695a419e5fd6
caroline.solievna@ideesculture.com <caroline.solievna@ideesculture.com>
2015-04-21 10:38:27 +0200

    search form improvement

--- app/controllers/search.js
+++ app/controllers/search.js
@@ -5,14 +5,19 @@
  * @uses core
  */
 var APP = require("core");
+var HIERARCHY_MODEL = require("models/ca-objects-hierarchy")();
+
+// Temporary fixing the table we"re editing, need to come through CONFIG after
+$.TABLE = "ca_objects";
+
 /**
  * Initializes the controller
  */
 $.init = function() {
 	APP.log("debug", "search.init");
 	
+	//navigation bar
 	$.NavigationBar.setBackgroundColor(APP.Settings.colors.primary);
-
 	if(APP.Settings.useSlideMenu) {
 		$.NavigationBar.showMenu(function(_event) {
 			APP.toggleMenu();
@@ -22,7 +27,19 @@ $.init = function() {
 			APP.removeChild(true);
 		});
 	}
+
+	// Initiating CA db model class
+	var info1 = APP.Settings.CollectiveAccess.urlForHierarchy.info1;
+	var info2 = APP.Settings.CollectiveAccess.urlForHierarchy.info2;
+	HIERARCHY_MODEL.init($.TABLE, info1, info2);
+
+	$.searchfield.addEventListener('change', $.search);
+
 };
+
+$.search = function(e){
+	alert(e.value);
+}
 
 
 // Kick off the init
