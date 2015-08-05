3b2e74a4cbdf9bcf6a64ec68d7f9a7274cb21bba
gautiermichelin <gm@ideesculture.com>
2015-08-05 14:30:06 +0200

    rebuilding clearcache functionality

--- app/controllers/settings_clearcache.js
+++ app/controllers/settings_clearcache.js
@@ -1,6 +1,6 @@
 /**
  * Controller for the settings clear cache screen
- * 
+ *
  * @class Controllers.settings.credits
  * @uses core
  */
@@ -9,9 +9,11 @@ var APP = require("core");
 
 var COMMONS = require("ca-commons");
 var CONFIG = arguments[0];
+Ti.API.log("debug","settings_clearcache CONFIG :");
+Ti.API.log("debug",CONFIG);
 var maxwidth = Ti.Platform.displayCaps.platformWidth;
 var maxheight = Ti.Platform.displayCaps.platformHeight;
-var HIERARCHY_MODEL = require("models/ca-objects-hierarchy")();
+//var HIERARCHY_MODEL = require("models/ca-objects-hierarchy")();
 
 /**
  * Initializes the controller
@@ -19,17 +21,35 @@ var HIERARCHY_MODEL = require("models/ca-objects-hierarchy")();
 $.init = function() {
 	//APP.log("debug", "settings_clearcache.init");
 
+	//$.NavigationBar.setBackgroundColor(APP.Settings.colors.primary);
+
+	/*$.NavigationBar.showBack(function(_event) {
+		//APP.removeChild(true);
+	});*/
+
+	//APP.rebuild();
 	$.NavigationBar.setBackgroundColor(APP.Settings.colors.primary);
 
-	$.NavigationBar.showBack(function(_event) {
-		APP.removeChild(true);
-	});
+	if(APP.Settings.useSlideMenu) {
+		$.NavigationBar.showMenu(function(_event) {
+			APP.toggleMenu();
+		});
+	} else {
+		$.NavigationBar.showBack(function(_event) {
+			APP.removeChild(true);
+		});
+	}
 
-	APP.rebuild();
-
-	$.label.text = "The local database was erased. Everything will now be fetched back again and cache reconstructed. Hit back to go back on home screen.";
+	$.label.text = "The local database will be erased. Everything will now be fetched back again and cache reconstructed. Hit menu to cancel and go back on home screen. This cannot be undone.";
 
 };
+
+$.clearcache.addEventListener("click", function(_event) {
+	APP.log("debug", "settings @emptycache");
+	APP.rebuild();
+
+});
+
 
 // Kick off the init
 $.init();
